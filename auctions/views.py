abe35a2ba3e26ef00369c5a0cf5e74c1390d1d99
Kamil Bednarski <kamilarturbednarski@gmail.com>
2020-12-02 18:27:21 +0100

    new: change password view

--- auctions/views.py
+++ auctions/views.py
@@ -1,18 +1,25 @@
 from django.contrib.auth import authenticate, login, logout
 from django.db import IntegrityError
 from django.http import HttpResponse, HttpResponseRedirect
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from django.urls import reverse
 from django.contrib.auth.decorators import login_required
+from django.contrib import messages
 
 from .models import Bid, Category, Contact, Comment, Listing, User
 
 
 def index(request):
+    '''
+    Renders index page.
+    '''
     return render(request, "auctions/index.html")
 
 
 def login_view(request):
+    '''
+    Logs user in.
+    '''
     if request.method == "POST":
 
         # Attempt to sign user in
@@ -34,11 +41,17 @@ def login_view(request):
 
 @login_required
 def logout_view(request):
+    '''
+    Logs user out.
+    '''
     logout(request)
     return HttpResponseRedirect(reverse("index"))
 
 
 def register(request):
+    '''
+    Registers new user.
+    '''
     if request.method == "POST":
         username = request.POST["username"]
         email = request.POST["email"]
@@ -72,6 +85,10 @@ def register(request):
 
 @login_required
 def profile_view(request):
+    '''
+    Renders profile page with user data.
+    '''
+    # Gets data about logged user from User model.
     logged_user = request.user
     id = logged_user.id
     username = logged_user.username
@@ -79,6 +96,7 @@ def profile_view(request):
     last_name = logged_user.last_name
     email = logged_user.email
     
+    # Gets additional data from Contact model.
     contact = Contact.objects.get(user_id=id)
     house = contact.house
     street = contact.street
@@ -101,7 +119,41 @@ def profile_view(request):
 
 @login_required
 def change_email(request):
+    '''
+    Changes email field in User model.
+    '''
     if request.method == 'POST':
-        return render(request, "auctions/change_email.html")
+
+        # Checks if all form inputs were submited.
+        if request.POST["email"] and request.POST["password"] and request.POST["password_confirmation"]:
+            email = request.POST["email"]
+            password = request.POST["password"]
+            confirmation = request.POST["password_confirmation"]
+            
+            # Checks if password and confirmation are equal.
+            if password != confirmation:
+                messages.info(request, "Password confirmation do not match password.") 
+                return redirect('change_email')
+
+            logged_user = request.user
+
+            # Password authentication.
+            check_password = authenticate(request, username=logged_user.username, password=password)
+
+            if check_password is not None:
+                # Update and save email field for logged user.
+                logged_user.email = email
+                logged_user.save()
+                messages.info(request, "E-mail succesfully updated.")
+                return redirect('profile')
+            else:
+                # If authentication failed, flashes message and redirects.
+                messages.info(request, "Password is uncorrect.") 
+                return redirect('change_email')
+                
+        else:
+            # If not all form inputs were submitted, flashes message and redirects.
+            messages.info(request, "You have to type new email, password and password confirmation.") 
+            return redirect('change_email')
     else:
         return render(request, "auctions/change_email.html")
